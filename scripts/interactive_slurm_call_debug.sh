7d7229c5baee3cbb15027acd9e44235a8392f605
Christian Baumgartner <c.f.baumgartner@gmail.com>
2021-04-29 17:01:21 +0200

    Change interactive slurm command into ralis training

--- scripts/interactive_slurm_call_debug.sh
+++ scripts/interactive_slurm_call_debug.sh
@@ -11,26 +11,21 @@ exec_command="singularity exec --nv --bind $data_path $sif_path"
 
 srun --pty --partition=gpu-2080ti-dev --gres=gpu:1 --mem=50G $exec_command \
         python3 -u $code_path/run.py \
-           --exp-name 'RALIS_camvid_test_seed'$seed \
-           --al-algorithm 'ralis' \
-           --checkpointer  \
-           --region-size 80 90 \
-           --ckpt-path $ckpt_path \
-           --data-path $data_path \
-           --load-weights \
-           --dataset 'camvid' \
-           --lr 0.001 \
-           --train-batch-size 32 \
-           --val-batch-size 4 \
-           --patience 150 \
-           --input-size 224 224 \
-           --only-last-labeled \
-           --budget-labels $budget \
-           --num-each-iter 24 \
-           --rl-pool 10 --seed $seed \
-           --train \
-           --test \
-           --final-test \
-           --exp-name-toload-rl 'RALIS_camvid_train_seed'$seed
-
-
+        --exp-name 'RALIS_cs_train_debug' \
+        --full-res \
+        --region-size 128 128 \
+        --snapshot 'best_jaccard_val.pth' \
+        --al-algorithm 'ralis' \
+        --ckpt-path $ckpt_path \
+        --data-path $data_path \
+        --dataset 'cityscapes' \
+        --lr 0.0001 \
+        --train-batch-size 16 \
+        --val-batch-size 1 \
+        --patience 10 \
+        --input-size 256 512 \
+        --only-last-labeled \
+        --budget-labels 3840  \
+        --num-each-iter 256 \
+        --rl-pool 20 \
+        --seed 1
